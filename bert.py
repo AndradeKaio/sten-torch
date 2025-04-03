bf260010bfbae45c7cc7accf3e78dca836f7ddc6
Kaio Henrique <deandradekaio@gmail.com>
2025-04-03 14:06:21 -0300

    chore: device print

--- bert.py
+++ bert.py
@@ -1,10 +1,10 @@
 import torch, time, sten
-from .sparsifier import RandomSparsifier
+from sparsifier import RandomSparsifier
 
 
 
 def bench_model(model, input, test_name: str, n: int = 1) -> float:
-    print(f"======== {test_name}-{model.name}=========")
+    print(f"========={test_name}-bert=========")
     total = 0
     for _ in range(n):
         start = time.time()
@@ -19,7 +19,6 @@ def build_sten(model):
     for module_name, module in model.named_modules():
         if isinstance(module, torch.nn.modules.linear.Linear):
             weight = module_name + ".weight"
-            weights_to_sparsify.append(weight)
             sb.set_weight(
                 name=weight,
                 initial_sparsifier=RandomSparsifier(0.10),
@@ -29,15 +28,18 @@ def build_sten(model):
     return sb.get_sparse_model(model)
 
 def main():
+    device = torch.device("cuda" if torch.cuda.is_available() and sys.argv[1] == "cuda" else "cpu")
+    print(f"========={device}=========")
+
     model = torch.hub.load('huggingface/pytorch-transformers',
         'model', 'bert-base-uncased').encoder.layer[0]
 
     torch.manual_seed(123)
     input_shape = (8, 128, 768)
-    input = torch.rand(input_shape)
+    input = torch.rand(input_shape, device=device)
 
     bench_model(model, input, "torch")
-    bench_model(build_sten(model), input, "torch")
+    bench_model(build_sten(model), input, "sten")
 
 if __name__ == "__main__":
     main()
