c870cd2be7a3d7c8268b76cb4b7f4b18f1e13537
Kaio Henrique <deandradekaio@gmail.com>
2025-04-04 12:35:18 -0300

    chore: rename test files

--- bert.py
+++ bert.py
@@ -38,35 +38,37 @@ def bench_model(model, input_t, test_name: str, n: int = 1) -> float:
     print("time:", final)
     return final
 
-def build_sten(model, sparsity: float = 0.1):
+def build_sten(model, sparsity: float = 0.1, pruning: str = "urandom"):
     sb = sten.SparsityBuilder()
     for module_name, module in model.named_modules():
         if isinstance(module, torch.nn.modules.linear.Linear):
             weight = module_name + ".weight"
             sb.set_weight(
                 name=weight,
-                initial_sparsifier=RandomSparsifier(sparsity),
+                initial_sparsifier=RandomSparsifier(sparsity, pruning),
                 tmp_format=torch.Tensor,
                 out_format=sten.CsrTensor,
             )
     return sb.get_sparse_model(model)
 
-def run_bench(name: str, number_of_runs: int, sparsity_levels: List[float], model, input_f, device):
-    with open(f"bert-base-uncased-{name}.txt", "wt") as file:
+def run_sten_bench(pruning: str, test_name: str, number_of_runs: int, sparsity_levels: List[float], model, input_f, device):
+    with open(f"{pruning}-{test_name}-{device}.txt", "wt") as file:
         for sparsity in sparsity_levels:
             t = bench_model(
                     build_sten(
                         model=model,
-                        sparsity=sparsity
+                        sparsity=sparsity,
+                        pruning=pruning,
                     ),
                     input_t=input_f(device),
-                    test_name=name,
+                    test_name=test_name,
                     n=number_of_runs
             )
             file.write(f"sparsity:{sparsity}, time:{t}\n")
 
 def main():
     device_arg = sys.argv[1] if len(sys.argv) > 1 else None
+    pruning = sys.argv[2] if len(sys.argv) > 2 else "urandom"
     device = torch.device("cuda" if torch.cuda.is_available() and device_arg == "cuda" else "cpu")
     print(f"========={device}=========")
 
@@ -81,8 +83,9 @@ def main():
     sparsity_levels = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99]
 
     for model, input_func, name in model_list:
-        run_bench(
-            name=name,
+        run_sten_bench(
+            pruning=pruning,
+            test_name=name,
             number_of_runs=5,
             sparsity_levels=sparsity_levels,
             model=model,
